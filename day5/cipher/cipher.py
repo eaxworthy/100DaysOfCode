8c9d1dbf8b0376167ed52d78ac612d248dc67a0c
Lizzy <axworthy@sonoma.edu>
2021-06-11 19:43:44 -0700

    optimized cipher slightly

--- day5/cipher/cipher.py
+++ day5/cipher/cipher.py
@@ -2,8 +2,10 @@
 
 letters ='abcdefghijklmnopqrstuvwxyz'
 
-def encrypt(message, shift_num):
+def caesar(message, shift_num, mode):
     result = ''
+    if mode == 'decode':
+        shift_num *= -1
     for char in message:
         if char.isalpha():
             c = letters[(letters.find(char.lower())+ shift_num)%26]
@@ -12,30 +14,18 @@ def encrypt(message, shift_num):
             result += char
     return result
 
-def decrypt(message, shift_num):
-    result = ''
-    for char in message:
-        if char.isalpha():
-            c = letters[(letters.find(char.lower()) - shift_num)%26]
-            result += c
-        else:
-            result += char
-    return result
+def main():
+    mode = input('Type \'encode\' to encrypt, type \'decode\' to decrypt:\n').lower()
 
+    while mode not in ['encode', 'decode']:
+        mode = input('Invalid Option Selected. Enter \'encode\' or \'decode\': ')
 
-option = input('Type \'encode\' to encrypt, type \'decode\' to decrypt:\n').lower()
+    message = input('Type your message:\n')
 
-while option not in ['encode', 'decode']:
-    option = input('Invalid Option Selected. Enter \'encode\' or \'decode\': ')
+    shift_num = int(input('Type the shift number:\n'))
 
-message = input('Type your message:\n')
+    print('Here is the ' + mode +'ed result: ' + caesar(message, shift_num, mode))
+    if input('Would you like to continue?(y/n): ') == 'y':
+        main()
 
-shift_num = int(input('Type the shift number:\n'))
-
-if option == 'encode':
-    print('Here is the encoded result: ' + encrypt(message, shift_num))
-else:
-    print('Here is the decoded result: ' + decrypt(message, shift_num))
-
-
-
+main()
