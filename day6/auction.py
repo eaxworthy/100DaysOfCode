9afbeaab3b00fd2f0e00dd15ff25535d49f9020e
Lizzy <axworthy@sonoma.edu>
2021-06-12 18:35:10 -0700

    Changed from recursive to loop solution.

--- day6/auction.py
+++ day6/auction.py
@@ -6,15 +6,17 @@ bidders = {}
 def get_bidder():
     name = input('What is your name?: ')
     bidders[name] = int(input('What is you bid??: $'))
-    if input('Are there any other bidders? Type \'yes\' or \'no\'.\n')== 'yes':
-        clear()
-        get_bidder()
+    return input('Are there any other bidders? Type \'yes\' or \'no\'.\n')
 
 
 print('Welcome to the secret auction program.')
-get_bidder()
+more_bidders = 'yes'
+
+while more_bidders == 'yes':
+    more_bidders = get_bidder()
+    clear()
+
 clear()
-print(bidders.get)
 winner = max(bidders, key=bidders.get)
 print('The winner is ' + winner + f' with a bid of ${bidders[winner]}.')
 
