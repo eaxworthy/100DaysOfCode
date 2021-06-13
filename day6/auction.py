ff4fb5acf99aa70c96c122b3a2bef5823e720771
Lizzy <axworthy@sonoma.edu>
2021-06-12 18:42:04 -0700

    removed redundant function and clear call, fixed a type.

--- day6/auction.py
+++ day6/auction.py
@@ -3,20 +3,15 @@ import os
 clear = lambda: os.system('clear')
 bidders = {}
 
-def get_bidder():
-    name = input('What is your name?: ')
-    bidders[name] = int(input('What is you bid??: $'))
-    return input('Are there any other bidders? Type \'yes\' or \'no\'.\n')
-
-
 print('Welcome to the secret auction program.')
 more_bidders = 'yes'
 
 while more_bidders == 'yes':
-    more_bidders = get_bidder()
+    name = input('What is your name?: ')
+    bidders[name] = int(input('What is you bid?: $'))
+    more_bidders = input('Are there any other bidders? Type \'yes\' or \'no\'.\n')
     clear()
 
-clear()
 winner = max(bidders, key=bidders.get)
 print('The winner is ' + winner + f' with a bid of ${bidders[winner]}.')
 
