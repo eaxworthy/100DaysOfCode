7c1e938d7ce420ef381207f59615f80db918331a
Lizzy <axworthy@sonoma.edu>
2021-06-14 20:37:25 -0700

    Final code of game.

--- day8/higher_lower.py
+++ day8/higher_lower.py
@@ -1,11 +1,40 @@
 import random
+import os
 from game_data import data
 
-#assign the total number of entries in our data file to a variable
-total_cards = len(data)
+clear = lambda: os.system('clear')
 
-#create a random shuffling of our instagram personalities
-deck = random.sample(data, total_cards)
+def game():
 
-choice = deck.pop()
-print(choice['name'] + ' ' + deck[0]['name'])
+    #assign the total number of entries in our data file to a variable
+    total_cards = len(data)
+
+    #create a random shuffling of our instagram personalities
+    deck = random.sample(data, total_cards)
+
+    score = 0
+
+    while len(deck) > 1:
+        card = deck.pop(0)
+        a = card['follower_count']
+        b = deck[0]['follower_count']
+        print(f"A: {card['name']}, a {card['description']} from {card['country']}.")
+        print("OR")
+        print(f"B: {deck[0]['name']}, a {deck[0]['description']} from {deck[0]['country']}.")
+        choice = input('Who has more followers? \'A\' or \'B\': ').lower()
+        if (choice == 'a' and a > b) or (choice == 'b' and b > a):
+            clear()
+            score += 1
+            print(f'You\'re right! Current score is {score}.')
+        else:
+            clear()
+            print(f'Sorry, but that was incorrect. Your final score was {score}')
+            return input('Play again?(y/n): ')
+            exit()
+
+    print(f'You guessed all {total_cards} entries right! Good Job!')
+
+play = 'y'
+while play == 'y':
+    clear()
+    play = game()
