1f8de4b1b0c5e734be7767ba82e9ff6c1b4ad2f1
Lizzy <axworthy@sonoma.edu>
2021-06-15 13:55:56 -0700

    Final coffee machine code.

--- day9/coffee_machine.py
+++ day9/coffee_machine.py
@@ -13,23 +13,24 @@ def process_order(order):
         if drinks[order]['water'] <= mr['water']:
             if drinks[order]['coffee'] <= mr['coffee']:
                 if drinks[order]['milk'] <= mr['milk']:
-                    new_money_total = make_change()
-                    return { 'water': mr['water'] - drinks[order]['water'],
-                            'coffee': mr['coffee'] - drinks[order]['coffee'],
-                            'milk': mr['milk'] - drinks[order]['milk'],
-                            'money': 0
-                            }
+                    if make_change(drinks[order]['cost']):
+                        print(f'Here is your {order}. Enjoy!')
+                        return { 'water': mr['water'] - drinks[order]['water'],
+                                'coffee': mr['coffee'] - drinks[order]['coffee'],
+                                'milk': mr['milk'] - drinks[order]['milk'],
+                                'profit': mr['profit'] + drinks[order]['cost']
+                                }
                 else:
-                    print('Sorry, thee is not enough milk.')
+                    print('Sorry, there is not enough milk.')
             else:
-                print('Sorry, thee is not enough coffee.')
+                print('Sorry, there is not enough coffee.')
         else:
-            print('Sorry, thee is not enough water.')
+            print('Sorry, there is not enough water.')
     else:
         print('Invalid Order. Please try again.')
     return mr
 
-def make_change():
+def make_change(cost):
     '''Prompts user for money given in form of number of coins. Calculates
     total ammount of money, compares against price of drink, and reports
     value of change given back. Returns a value for the total ammount of
@@ -39,15 +40,20 @@ def make_change():
     dimes = int(input('How many dimes?: '))
     nickles = int(input('How many nickles?: '))
     pennies = int(input('How many pennies?: '))
-    total_given = round(quarters*0.25 + dimes*0.1 + nickles*0.05 + pennies*0.01, 2)
-    print(total_given)
-
+    total_given = quarters*0.25 + dimes*0.1 + nickles*0.05 + pennies*0.01
+    if total_given >= cost:
+        change_total = round(total_given - cost, 2)
+        print(f'Here is ${change_total} dollars in change.')
+        return True
+    else:
+        print('Sorry, insufficient change. Money refunded.')
+        return False
 
 def report():
     '''Return's a report on the current resources of the running coffee
     machine instance'''
 
-    print(f"Water: {mr['water']}ml\nMilk: {mr['milk']}ml\nCoffee: {mr['coffee']}ml\nMoney: ${mr['money']}")
+    print(f"Water: {mr['water']}ml\nMilk: {mr['milk']}ml\nCoffee: {mr['coffee']}ml\nMoney: ${mr['profit']}")
 
 on = True
 print(drink_list)
@@ -56,5 +62,7 @@ while on:
     order = input('What would you like? (espresso/latte/cappuccino): ')
     if order == 'report':
         report()
+    elif order == 'off':
+        on = False
     else:
         mr = process_order(order)
