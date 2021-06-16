c97902b6402ad20a5da649f99f9956c4ffe63021
Lizzy <axworthy@sonoma.edu>
2021-06-16 11:19:35 -0700

    Final code version of class based coffee machine.

--- day9/oop_coffee_machine/main.py
+++ day9/oop_coffee_machine/main.py
@@ -1,3 +1,25 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+
+
+coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine()
+menu = Menu()
+
+machine_on = True
+
+while machine_on:
+    order = input(f'What would you like to order? ({menu.get_items()}): ')
+    if order == 'off':
+        machine_on = False
+    elif order == 'report':
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        drink = menu.find_drink(order)
+        if drink:
+            if coffee_maker.is_resource_sufficient(drink):
+                money_machine.make_payment(drink.cost)
+                coffee_maker.make_coffee(drink)
