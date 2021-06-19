7b6da00832dc03a80f08ac44710100ccffd705ba
Lizzy <axworthy@sonoma.edu>
2021-06-19 13:54:59 -0700

    Improved animation and enabled turning

--- day13/snake.py
+++ day13/snake.py
@@ -9,16 +9,18 @@ class Snake():
 
     def __init__(self):
         
-        for i in range(0,3):
-            t = Turtle()
+        for i in range(0,6):
+            t = Turtle(shape='square')
             t.penup()
+            t.speed(0)
             t.color('white','white')
-            t.shape('square')
+            
 
             #default size of a square turtle is 20x20 px. By overlapping
             #segments, we can create a smoother animation.
             t.setx(-10*i)
             self.body.append(t)
+        self.head = self.body[0]
     
     def add_tail(self, pos):
         t = Turtle()
@@ -35,21 +37,44 @@ class Snake():
         self.add_tail(pos)
 
     def move(self):
-        for segment in self.body:
-            segment.forward(10)
+        new_pos = [self.head.pos()]
+        self.head.forward(10)
+        screen.delay(10)
+        for segment in self.body[1:]:
+            new_pos.append(segment.pos())
+            segment.setpos(new_pos.pop(0))
    
+
+    def turn(self, direction):
+        #new_pos = [self.head.pos()]
+        #screen.delay(25)
+        self.head.setheading(direction)
+        #self.head.forward(10)
+        #for segment in self.body[1:]:
+        #    new_pos.append(segment.pos())
+        #    segment.setpos(new_pos.pop(0))
+        #    segment.setheading(direction)
+
     body = []
+    pos = [(0,0)]
+    direction = 0
 
 
-def add_tail():
-    pass
 
 def new_food():
     pass
 
 snake = Snake()
-snake.eat()
-for i in range(0,20):
+
+screen.onkeypress(lambda: snake.turn(90), 'Up')
+screen.onkeypress(lambda: snake.turn(180), 'Left')
+screen.onkeypress(lambda: snake.turn(270), 'Down')
+screen.onkeypress(lambda: snake.turn(0), 'Right')
+
+screen.listen()
+
+while True:
     snake.move()
-snake.eat()
+
+
 screen.exitonclick()
