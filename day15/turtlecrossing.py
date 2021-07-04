d46e824ad203142fb3a25f9c1a27e571d1862401
Lizzy <axworthy@sonoma.edu>
2021-07-04 11:38:08 -0700

    Car generation and basic movement behavior done.

--- day15/turtlecrossing.py
+++ day15/turtlecrossing.py
@@ -11,34 +11,40 @@ class Car(Turtle):
         super().__init__()
         self.hideturtle()
         self.penup()
-        self.setpos(SCREEN_WIDTH/2, randint(-(SCREEN_HEIGHT/2-50), SCREEN_HEIGHT/2-50))
+        self.setpos(SCREEN_WIDTH/2, randint(-(SCREEN_HEIGHT/2-75), SCREEN_HEIGHT/2-50))
         self.setheading(180)
         self.movement_speed = randint(5,15)
         self.color(COLORS[randint(0,len(COLORS)-1)])
         self.shape('square')
         self.shapesize(stretch_len=1.5, stretch_wid=1.0)
         self.showturtle()
+    
+    def offscreen(self):
+        if self.xcor() < -(SCREEN_WIDTH/2 - 10):
+            return True
+        return False
+    
+    def reset(self):
+        self.setpos(SCREEN_WIDTH/2, randint(-(SCREEN_HEIGHT/2-75), SCREEN_HEIGHT/2-50))
 
     def move(self):
-        self.forward(self.movement_speed)
+        if not self.offscreen():
+            self.forward(self.movement_speed)
+        else:
+            self.reset()
 
-    def reset(self):
-        self.setpos(SCREEN_WIDTH/2, randint(-(SCREEN_HEIGHT/2-50), SCREEN_HEIGHT/2-50))
-
-car = Car()
 screen = Screen()
 screen.setup(width=SCREEN_WIDTH, height=SCREEN_HEIGHT)
 screen.tracer(False)
-
+cars = []
+cars.extend([Car() for i in range(10)])
 screen.update()
-for i in range(5):
-    for j in range(20):
+
+while True:
+    for car in cars:
         car.move()
-        screen.update()
-        sleep(0.05)
-    car.reset()
     screen.update()
-    sleep(0.1)
+    sleep(0.05)
 
 screen.exitonclick()
 
