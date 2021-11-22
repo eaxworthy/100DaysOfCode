89dca4ff21028b1fcd3705ea0db3a201797c4912
eaxworthy <lizzieaxworthy@gmail.com>
2021-11-22 10:19:28 -0800

    Minor changes to window config.

--- pomo/main.py
+++ pomo/main.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 
-WINDOW_HEIGHT = 390
-WINDOW_WIDTH = 410
+WINDOW_HEIGHT = 350
+WINDOW_WIDTH = 270
 PINK = '#e2979c'
 RED = '#e7305b'
 GREEN = '#9bdeac'
@@ -15,7 +15,7 @@ LONG_BREAK_MIN = 20
 window = tk.Tk()
 window.title('Pomodoro Timer')
 window.minsize(width=WINDOW_WIDTH, height=WINDOW_HEIGHT)
-window.config(padx=100, pady=40, bg=GREEN)
+window.config(padx=25, pady=20, bg=GREEN)
 tomato_img = tk.PhotoImage(file='images/tomato.png')
 canvas = tk.Canvas(width=(tomato_img.width())+2, height=tomato_img.height(), bg=GREEN, highlightthickness=0)
 
