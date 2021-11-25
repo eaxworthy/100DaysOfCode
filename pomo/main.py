39394f9d9825ae0efe68515c686730ee85d44b7b
Lizzy <axworthy@sonoma.edu>
2021-11-24 22:08:31 -0800

    Final cleanup and documentation.

--- pomo/main.py
+++ pomo/main.py
@@ -1,15 +1,17 @@
 import tkinter as tk
 import time
 
+#Display settings
 WINDOW_HEIGHT = 360
 WINDOW_WIDTH = 290
-PINK = '#e2979c'
-RED = '#e7305b'
-GREEN = '#9bdeac'
+RED = '#f26849'
+GREEN = '#379b46'
 YELLOW = '#f1ffc4'
 FONT_NAME = 'clean'
 BUTTON_FONT = ('Arial',12, "normal")
 
+#Time settings
+#Adjust however best fits you
 WORK_MIN = 1
 SHORT_BREAK_MIN = 1
 LONG_BREAK_MIN = 30
@@ -19,36 +21,42 @@ START_TIMES = [WORK_MIN, SHORT_BREAK_MIN, LONG_BREAK_MIN]
 class Pomo:
     def __init__(self):
 
+        #state management variables
         self.pom_count = 0
         self.current_mode = 0
         self.minutes = WORK_MIN
         self.seconds = 0
         self.running = False
 
-        #setup window
+        #setup app window
         self.root = tk.Tk()
         self.root.title('Pomodoro Timer')
         self.root.minsize( width=WINDOW_WIDTH, height=WINDOW_HEIGHT)
         self.root.config(bg=YELLOW)
 
-        #Setup tomato image
+        #setup tomato image
         tomato_img = tk.PhotoImage(file='images/tomato.png')
         self.canvas = tk.Canvas(width=WINDOW_WIDTH, height=tomato_img.height(), \
                                 bg=YELLOW, highlightthickness=0)
-        self.canvas.create_image(WINDOW_WIDTH//2, tomato_img.height()//2, image=tomato_img)
-        self.timer_display = self.canvas.create_text(WINDOW_WIDTH//2, 140, text=str(self.minutes).zfill(2)+':'+str(self.seconds).zfill(2), fill="white",
-                                font=(FONT_NAME, 28, "bold"))
+        self.canvas.create_image(WINDOW_WIDTH//2, tomato_img.height()//2, \
+                                image=tomato_img)
 
-        #Setup text displays
-        self.counter = tk.Label(self.root, text=str(self.pom_count), fg=RED, bg=YELLOW, font=('Arial', 14, "bold"))
-        self.mode_display = tk.Label(self.root, text = MODES[self.current_mode], bg=YELLOW, fg=RED, font=('Arial', 20, "bold"))
+        #create timer
+        self.timer_display = self.canvas.create_text(WINDOW_WIDTH//2, 140, \
+                                text=str(self.minutes).zfill(2)+':'+str(self.seconds).zfill(2),
+                                fill="white", font=(FONT_NAME, 28, "bold"))
+
+        #setup text displays
+        self.counter = tk.Label(self.root, text=str(self.pom_count), fg=RED, \
+                                bg=YELLOW, font=('Arial', 14, "bold"))
+        self.mode_display = tk.Label(self.root, text = MODES[self.current_mode], \
+                                    bg=YELLOW, fg=RED, font=('Arial', 20, "bold"))
 
         #Setup buttons
-        self.start_button = tk.Button(self.root, command=self.start_timer, text='Start', width=6, bg=GREEN,  font=BUTTON_FONT)
-        self.pause_button = tk.Button(self.root, command=self.pause_timer, text='Pause', width=6, bg=RED, font=BUTTON_FONT)
-       # self.start_button.bind("<Button>", start_timer)
-       # self.pause_button.bind("<Button>", pause_timer)
-
+        self.start_button = tk.Button(self.root, command=self.start_timer, \
+                                      text='Start', width=6, bg=GREEN,  font=BUTTON_FONT)
+        self.pause_button = tk.Button(self.root, command=self.pause_timer, \
+                                      text='Pause', width=6, bg=RED, font=BUTTON_FONT)
 
         #Layout with grid
         self.mode_display.grid(column=0, row = 0, pady=20, sticky='S', columnspan=5)
@@ -56,16 +64,18 @@ class Pomo:
         self.start_button.grid(column=0, row=3, columnspan=1, rowspan=1, pady=10, padx=20, sticky="W")
         self.pause_button.grid(column=4, row=3, columnspan=1, rowspan=1, pady=10, padx=20, sticky="E")
         self.counter.grid(column=1, row=3, columnspan=3)
-        
+
         #Start window
         self.root.mainloop()
 
     #Define button functions
+    #start timer running
     def start_timer(self):
         if not self.running:
             self.running = True
             self.run_timer()
 
+    #pause timer, kill running process
     def pause_timer(self):
         if self.running:
             self.root.after_cancel(self.after_id)
@@ -78,36 +88,44 @@ class Pomo:
             return
 
         #update display
+
         if not self.seconds and not self.minutes:
-            #update mode
+            #end of period, update mode
+
+            #if last period was a work period, see what kind of rest is next
             if not self.current_mode:
+                self.mode_display['fg'] = GREEN
                 self.pom_count += 1
                 if not self.pom_count % 4:
                     self.current_mode = 2
                 else:
                     self.current_mode = 1
 
+            #set back to work mode
             else:
+                self.mode_display['fg'] = RED
                 self.current_mode = 0
-                
+
+            #update display
             self.mode_display['text'] = MODES[self.current_mode]
-            self.minutes = START_TIMES[self.current_mode] 
-        
-        elif not self.seconds:  
+            self.minutes = START_TIMES[self.current_mode]
+
+        elif not self.seconds:
+            #Start new minute
             self.minutes = self.minutes-1
-            self.seconds = 59       
-        
-        else: 
+            self.seconds = 59
+
+        else:
+            #normal second deprecation
             self.seconds = self.seconds - 1
-        
+
+        #update display
         self.canvas.itemconfigure(self.timer_display, text=str(self.minutes).zfill(2)+':'+str(self.seconds).zfill(2))
         self.counter['text'] = str(self.pom_count)
-        self.after_id = self.root.after(1000, self.run_timer)
-    
+
+        #continue timer
+        self.after_id = self.root.after(100, self.run_timer)
+
 
 if __name__ ==  "__main__":
     pomo = Pomo()
-
-
-
-
